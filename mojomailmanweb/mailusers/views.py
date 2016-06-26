1a63c0805b5fcf10c6b538f4a369b4a3076a08b5
Arjun Venkatraman <arjun@mojolab.org>
2016-06-26 16:34:34 +0530

    Updated

--- mojomailmanweb/mailusers/views.py
+++ mojomailmanweb/mailusers/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-
+from django.template import Context
+from mailusers.models import *
 @login_required
 def index(request):
-    return render(request,"mailusers/index.html")
+	c=Context()
+	emailaddresses=EmailAddress.objects.all().filter(user__username=request.user.username)
+	c['emailaddresses']=emailaddresses
+	print c
+	return render(request,"mailusers/index.html",c)
 
 # Create your views here.
